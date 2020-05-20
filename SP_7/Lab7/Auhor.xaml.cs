a4e6038663ab370a19cb42ba68344f42cba765cd
artem2315 <40527253+artem2315@users.noreply.github.com>
2020-05-20 20:07:10 +0300

    Add files via upload

---
+++ SP_7/Lab7/Auhor.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Lab7
+{
+    public sealed partial class Auhor : Page
+    {
+        public Auhor() =>
+            this.InitializeComponent();
+
+        private void BackToMainPage_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.GoBack();
+        }
+    }
+}
---
+++ SP_7/Lab7/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace Lab7
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage() =>
+            this.InitializeComponent();
+
+        private void ContentHub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e)
+        {
+            switch (e.Section.Name)
+            {
+                case "mainSection": { this.Frame.Navigate(typeof(MainPage)); } break;
+                case "convertorSection": { this.Frame.Navigate(typeof(Conver)); } break;
+                case "aboutAuthorSection": { this.Frame.Navigate(typeof(Auhor)); } break;
+            }
+        }
+    }
+}
